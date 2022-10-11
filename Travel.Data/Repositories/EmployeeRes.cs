8b3f59c77e186f6c8ac8f6c8519400c9bfc6678c
hienpt9602@gmail.com <hienpt9602@gmail.com>
2022-10-11 20:26:06 +0700

    fixx

--- Travel.Data/Repositories/EmployeeRes.cs
+++ Travel.Data/Repositories/EmployeeRes.cs
@@ -351,32 +351,32 @@ namespace Travel.Data.Repositories
             }
         }
 
-        //public Response ViewDelete()
-        //{
-        //    try
-        //    {
-        //        var listEmployee = (from x in _db.Employees where x.IsDelete == true select x).ToList();
-        //        var result = Mapper.MapEmployee(listEmployee);
-        //        if (result.Count() > 0)
-        //        {
-        //            res.Content = result;
-        //        }
-        //        else
-        //        {
-        //            res.Notification.DateTime = DateTime.Now;
-        //            res.Notification.Messenge = "Không có dữ liệu trả về !";
-        //            res.Notification.Type = "Warning";
-        //        }
-        //        return res;
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        res.Notification.DateTime = DateTime.Now;
-        //        res.Notification.Description = e.Message;
-        //        res.Notification.Messenge = "Có lỗi xảy ra !";
-        //        res.Notification.Type = "Error";
-        //        return res;
-        //    }
-        //}
+        public Response ViewDelete()
+        {
+            try
+            {
+                var listEmployee = (from x in _db.Employees where x.IsDelete == true select x).ToList();
+                var result = Mapper.MapEmployee(listEmployee);
+                if (result.Count() > 0)
+                {
+                    res.Content = result;
+                }
+                else
+                {
+                    res.Notification.DateTime = DateTime.Now;
+                    res.Notification.Messenge = "Không có dữ liệu trả về !";
+                    res.Notification.Type = "Warning";
+                }
+                return res;
+            }
+            catch (Exception e)
+            {
+                res.Notification.DateTime = DateTime.Now;
+                res.Notification.Description = e.Message;
+                res.Notification.Messenge = "Có lỗi xảy ra !";
+                res.Notification.Type = "Error";
+                return res;
+            }
+        }
     }
 }
