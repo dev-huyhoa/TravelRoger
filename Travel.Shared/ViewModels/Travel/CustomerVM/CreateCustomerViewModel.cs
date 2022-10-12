4e0815016c4023d48cfc50906cddccf7c71124e8
Dev_HuyHoa <driverhuyoa@gmail.com>
2022-10-12 21:45:45 +0700

    customer

---
+++ Travel.Shared/ViewModels/Travel/CustomerVM/CreateCustomerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel.Shared.ViewModels.Travel.CustomerVM
+{
+    public class CreateCustomerViewModel
+    {
+        private string nameCustomer;
+        private string phone;
+        private string email;
+        private string address;
+        private bool gender;
+        private string password;
+        private long birthday;
+
+        public string NameCustomer { get => nameCustomer; set => nameCustomer = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public string Email { get => email; set => email = value; }
+        public string Address { get => address; set => address = value; }
+        public string Password { get => password; set => password = value; }
+        public long Birthday { get => birthday; set => birthday = value; }
+        public bool Gender { get => gender; set => gender = value; }
+    }
+}
