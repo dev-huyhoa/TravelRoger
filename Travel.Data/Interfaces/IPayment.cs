ab8528e0235722e1dcdf8e059793783b8abd0faf
sang ngoc <ngocsang4tvtvp@gmail.com>
2022-10-13 21:32:41 +0700

    create - get Payment

---
+++ Travel.Data/Interfaces/IPayment.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travel.Shared.ViewModels;
+using Travel.Shared.ViewModels.Travel;
+
+namespace Travel.Data.Interfaces
+{
+    public interface IPayment
+    {
+        CreateUpdatePaymentViewModel CheckBeforSave(JObject frmData, ref Notification _message);
+        Response Gets();
+        Response Create(CreateUpdatePaymentViewModel input);
+        Response Update(CreateUpdatePaymentViewModel input);
+    }
+}
